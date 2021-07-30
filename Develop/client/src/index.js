9511da38afc4c1162c7f0f24339d6d168680a5f5
dsanchezjr99 <77306064+dsanchezjr99@users.noreply.github.com>
2021-07-30 00:49:05 -0400

    Update index.js

--- Develop/client/src/index.js
+++ Develop/client/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+
+
 
 ReactDOM.render(
   <React.StrictMode>
